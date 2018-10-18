f9e3724a919681c7e7b097b0f256bc867a9ef6b9
azou <Alanuuu@163.com>
2018-10-18 17:41:02 +0800

    feat: add search component

---
+++ examples/pages/search/index.js
@@ -0,0 +1,21 @@
+//Page Object
+Page({
+    data: {
+        value: ''
+    },
+    onBlur() {
+        console.log('blur');
+    },
+    // eslint-disable-next-line
+    onChange({detail}) {
+        this.setData({
+            value: detail.value
+        });
+    },
+    onFocus() {
+        console.log('focus');
+    },
+    onConfirm({detail}) {
+        console.log(detail.value);
+    }
+});
